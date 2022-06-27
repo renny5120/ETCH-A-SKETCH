f7184e8fae655f4fafbb77acb3f3e7313224ab65
renny5120 <renny5120@gmail.com>
2022-06-27 16:09:33 -0700

    add grid creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ETCH-A-SKETCH
+This program will display something similar to a sketch pad. Users will be able to draw on a grid with their mouse. 
+They will be able to set the size of the grid to a max of 100 boxes. Most of the grid will be created through javascript part of this project.
---
+++ javascript.js
@@ -0,0 +1,19 @@
+// creates boxes
+function createGrid(num) {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        // fills row with num boxes
+        for (let j = 0; j < num; j++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            row.appendChild(box);
+        }
+        // adds row to container
+        container.appendChild(row);
+    }
+};
+
+createGrid(16);
