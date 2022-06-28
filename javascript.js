571b556c364795db3c725b1300bec0f613e4a208
renny5120 <renny5120@gmail.com>
2022-06-28 14:14:23 -0700

    add grid resizer

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+// grid size button
+const body = document.querySelector('body');
+const btn = document.createElement('button');
+btn.classList.add('resize');
+btn.textContent = 'Resize Grid';
+body.appendChild(btn);
+
 // creates grid boxes
 function createGrid(num) {
     const container = document.querySelector('.container');
@@ -18,3 +25,45 @@ function createGrid(num) {
 };
 
 createGrid(16);
+addBtnListener();
+
+// removes grid 
+function removeGrid() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        row.remove();
+    });
+}
+
+
+// button function
+const button = document.querySelector('.resize');
+button.addEventListener('click', () => {
+    let num = prompt('Please enter a number between 1-100');
+    // if user inputs invalid number
+    if (num > 100 || num < 1) {
+        alert('Invalid Number!')
+        return;
+    }
+    removeGrid();
+    createGrid(num);
+    addBtnListener();
+});
+
+// // button listener
+// const boxes = document.querySelectorAll('.box');
+// boxes.forEach((div) => {
+//     div.addEventListener('mouseover', function (e) {
+//         e.target.style.background = 'black';
+//     });
+// });
+
+function addBtnListener() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((div) => {
+        div.addEventListener('mouseover', function (e) {
+            e.target.style.background = 'black';
+        });
+    });
+}
+
